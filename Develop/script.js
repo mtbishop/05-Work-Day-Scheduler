ef04d9031df0674c2684bdbe03757ecc5856740f
mtbishop <bishopmatthew76@gmail.com>
2021-01-16 16:18:28 -0600

    formated documents, updated css, added comments for js

--- Develop/script.js
+++ Develop/script.js
@@ -64,89 +64,97 @@ var theDay = [
   },
 ];
 
+// gives you the current day of the week & month & day
 function getHeaderDate() {
   var currentHeaderDate = moment().format('dddd, MMMM Do');
   $('#currentDay').text(currentHeaderDate);
 }
 
+// saves the reminders in the day array in a string
 function saveReminders() {
   localStorage.setItem('theDay', JSON.stringify(theDay));
 }
 
+//
 function displayReminders() {
   theDay.forEach(function (_thisHour) {
     $(`#${_thisHour.id}`).val(_thisHour.reminder);
   });
 }
 
+// local storage function
 function init() {
-  var localDay = JSON.parse(localStorage.getItem('theDay'));
+  var storedDay = JSON.parse(localStorage.getItem('theDay'));
 
-  if (localDay) {
-    theDay = localDay;
+  if (storedDay) {
+    theDay = storedDay;
   }
 
   saveReminders();
   displayReminders();
 }
 
+// fetches header date
 getHeaderDate();
 
 // creates the visuals for the scheduler body
-theDay.forEach(function(thisHour) {
-  var hourRow = $("<form>").attr({
-    "class": "row"
+theDay.forEach(function (thisHour) {
+  var hourRow = $('<form>').attr({
+    class: 'row',
   });
-  $(".container").append(hourRow);
+  $('.container').append(hourRow);
 
   // crates time field
-  var hourField = $("<div>")
-  .text(`${thisHour.hour}${thisHour.meridiem}`)
-  .attr({
-    "class": "col-md-2 hour"
-  });
-  
-  var hourPlan = $("<div>")
-  .attr({
-    "class": "col-md-9 description p-0"
+  var hourField = $('<div>').text(`${thisHour.hour}${thisHour.meridiem}`).attr({
+    class: 'col-md-2 hour',
   });
 
-  var planData = $("<textarea>");
+  var hourPlan = $('<div>').attr({
+    class: 'col-md-9 description p-0',
+  });
+
+  // appends text area and links the class attributes based on time of day from momentjs
+  var planData = $('<textarea>');
   hourPlan.append(planData);
-  planData.attr("id", thisHour.id);
-  if (thisHour.time < moment().format("HH")) {
-    planData.attr( {
-      "class": "past",
-    })
-
-  }else if(thisHour.time === moment().format("HH")) {
+  planData.attr('id', thisHour.id);
+  if (thisHour.time < moment().format('HH')) {
     planData.attr({
-      "class": "present"
-    })
-  }else if (thisHour.time > moment().format("HH")) {
+      class: 'past',
+    });
+  } else if (thisHour.time === moment().format('HH')) {
     planData.attr({
-      "class": "future"
-    })
+      class: 'present',
+    });
+  } else if (thisHour.time > moment().format('HH')) {
+    planData.attr({
+      class: 'future',
+    });
   }
 
-  var saveButton = $("<i class='far fa-save fa-lg'></i>")
-  var savePlan = $("<button>")
-  .attr( {
-    "class": "col-md-1 saveBtn"
+  var saveButton = $("<i class='far fa-save fa-lg'></i>");
+  // attributes for save buttons
+  var savePlan = $('<button>').attr({
+    class: 'col-md-1 saveBtn',
   });
   savePlan.append(saveButton);
   hourRow.append(hourField, hourPlan, savePlan);
+});
 
-})
-
+// fetches local storage data
 init();
 
-
-$(".saveBtn").on("click", function(event) {
+// save button click function
+$('.saveBtn').on('click', function (event) {
   event.preventDefault();
-  var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-  theDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
-  console.log(saveIndex);
+  var saveIndex = $(this)
+    .siblings('.description')
+    .children('.future')
+    .attr('id');
+  theDay[saveIndex].reminder = $(this)
+    .siblings('.description')
+    .children('.future')
+    .val();
+
   saveReminders();
   displayReminders();
-})
+});
